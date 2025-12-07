bd22f507da6416b13493957d65ab08a1f5c5b31a
wrenbury <kenton@wrenburybrand.com>
2025-12-06 22:00:25 -0600

    Delete nfl-dashboard-backend-main directory

--- nfl-dashboard-backend-main/football_dash_frontend/tailwind.config.cjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html","./src/**/*.{ts,tsx}"],
-  theme: { extend: { colors: { panel:"#0b0d12"} } },
-  plugins: [],
-}
