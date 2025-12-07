77f636f4407921c7b5542bd47e99528638aba5a1
wrenbury <kenton@wrenburybrand.com>
2025-12-06 21:28:12 -0600

    Update vite.config.ts

--- football_dash_frontend/vite.config.ts
+++ football_dash_frontend/vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173, proxy: { '/api': 'http://localhost:8000' } }
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': 'http://localhost:8000',
+      '/cfb': 'http://localhost:8000',
+      '/games': 'http://localhost:8000',
+    },
+  },
 })
